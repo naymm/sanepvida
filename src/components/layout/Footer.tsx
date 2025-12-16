42119a1ebf947e11a953573310d560da250460e6
naym <medvanio4@gmail.com>
2025-12-16 11:11:13 +0100

    Actualização do Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -84,7 +84,7 @@ const Footer = () => {
               <div className="flex items-start gap-3">
                 <MapPin className="w-5 h-5 text-primary mt-1 shrink-0" />
                 <p className="text-footer-foreground/70">
-                  Vila Fon-Ar, Condomínio Frente da Caixa Cruz, Luanda, Angola
+                  Vila Flor, Condomínio Frente da Caixa Cruz, Luanda, Angola
                 </p>
               </div>
               <div className="flex items-center gap-3">
